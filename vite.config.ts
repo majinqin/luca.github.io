28726da807f975f3de366d7683c76a80ffff8e5a
maDing <ma.jinqin@synyi.com>
2024-09-26 14:56:51 +0800

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/luca.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
